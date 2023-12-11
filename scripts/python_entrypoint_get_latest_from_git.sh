44f8392d4388606f86fea9a1b1749a69115bc3e3
John Yavuzyilmaz <omercanyy@gmail.com>
2023-12-10 16:25:35 -0800

    fix script paths (#24)

--- scripts/python_entrypoint_get_latest_from_git.sh
+++ scripts/python_entrypoint_get_latest_from_git.sh
@@ -5,14 +5,11 @@ PROJECT_DIR="$HOME/HeadTrackingFPVRaspberryPi"
 PYTHON_SCRIPT="src.raspberry_pi_governer_scripts.get_latest_from_git"
 ALIAS_NAME="get_latest_from_git"
 
-# Construct the full path to the Python script
-PYTHON_SCRIPT_PATH="$PROJECT_DIR/$PYTHON_SCRIPT"
-
 # Check if the alias exists in ~/.bash_aliases
 if ! grep -q "alias $ALIAS_NAME=" ~/.bash_aliases; then
     echo "Adding alias '$ALIAS_NAME' to ~/.bash_aliases"
     # Add an alias that uses 'pipenv run'
-    echo "alias $ALIAS_NAME='cd $PROJECT_DIR && pipenv install && pipenv run python -m $PYTHON_SCRIPT_PATH'" >> ~/.bash_aliases
+    echo "alias $ALIAS_NAME='cd $PROJECT_DIR && pipenv install && pipenv run python -m $PYTHON_SCRIPT'" >> ~/.bash_aliases
     # Apply the changes to the current environment
     source "$HOME/.bashrc"
 else
--- scripts/python_entrypoint_setup_wifi_direct_connection.sh
+++ scripts/python_entrypoint_setup_wifi_direct_connection.sh
@@ -5,14 +5,11 @@ PROJECT_DIR="$HOME/HeadTrackingFPVRaspberryPi"
 PYTHON_SCRIPT="src.wifi_direct_setup.wifi_direct_manager"
 ALIAS_NAME="setup_wifi_direct_connection"
 
-# Construct the full path to the Python script
-PYTHON_SCRIPT_PATH="$PROJECT_DIR/$PYTHON_SCRIPT"
-
 # Check if the alias exists in ~/.bash_aliases
 if ! grep -q "alias $ALIAS_NAME=" ~/.bash_aliases; then
     echo "Adding alias '$ALIAS_NAME' to ~/.bash_aliases"
     # Add an alias that uses 'pipenv run'
-    echo "alias $ALIAS_NAME='cd $PROJECT_DIR && pipenv install && pipenv run python -m $PYTHON_SCRIPT_PATH'" >> ~/.bash_aliases
+    echo "alias $ALIAS_NAME='cd $PROJECT_DIR && pipenv install && pipenv run python -m PYTHON_SCRIPT'" >> ~/.bash_aliases
     # Apply the changes to the current environment
     source "$HOME/.bashrc"
 else
